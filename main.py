61cfa3773bc47c15fadee453326b2755c6579fdb
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-27 22:27:20 +0700

    set

---
+++ main.py
@@ -0,0 +1,19 @@
+#belajar set
+data= {"belajar", "mahasiswa", "unsulbar", "informatika"}
+print(type(data))
+
+
+data = {"belajar", "masiswa", "unsulbar", "informatika"}
+nilai = {100, 200, 300, 400}
+acak = {"Python", 200, 6.99, True}
+
+print(data)
+print(nilai)
+print(acak)
+
+
+angka = {1, 2, 3, 4, 5}
+nilai = {3, 4, 5, 6, 7}
+
+print (angka | nilai) # union
+print (angka & nilai) # intersection
